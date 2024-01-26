cee2f88bdc868ef25850039020a528d485d9674d
lksoumon <99751496+lksoumon@users.noreply.github.com>
2024-01-26 10:46:31 -0400

    Update Open4School_link_sigeduca.js

--- Open4School_link_sigeduca.js
+++ Open4School_link_sigeduca.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cadastro no Open4School
 // @namespace    http://tampermonkey.net/
-// @version      2024-01-23
+// @version      v1.2
 // @description  Importa os dados so sigeduca para o Open
 // @author       Lucas Monteiro
 // @match        http://sigeduca.seduc.mt.gov.br/ged/hwmconaluno.aspx
